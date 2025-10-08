97e3d52ec93216274d8afbc8d5f6900581009f67
Soyl <Soylgr@yandex.ru>
2025-10-08 15:13:28 +0300

    fix

--- src/main/java/testSaveMp/testSaveMp/config/SecurityConfig.java
+++ src/main/java/testSaveMp/testSaveMp/config/SecurityConfig.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,13 +20,13 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { //csrf(AbstractHttpConfigurer::disable);
         http
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/", "/index.html",
-                                        "/css/**", "/js/**").permitAll()
-                                .anyRequest().hasRole("USER"))
+                                .requestMatchers("/index.html",
+                                        "/", "/search/**", "/css/**", "/js/**").permitAll()
+                                .anyRequest().hasAnyRole("USER", "ADMIN"))
                 .formLogin(form -> form
                         .loginPage("/login").loginProcessingUrl("/perform_login")
                         .defaultSuccessUrl("/download.html", true)
-                        .failureUrl("/login?error=true").permitAll()
+                        .permitAll()
                 ).csrf(csrf -> csrf.ignoringRequestMatchers("/download"));
 
 
@@ -34,11 +35,20 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService () {
-        UserDetails user = User.withUsername("u")
+        UserDetails user = User.withUsername("User")
                 .password("{noop}2876")
                 .roles("USER")
                 .build();
+        UserDetails admin = User.withUsername("Admin")
+                .password(passwordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
     }
 }
